5dd4d9d7428cd0ce738b272f331559d7cc118b49
Mehedi Hasan <mehedi.hasan@dsinnovators.com>
2020-12-19 11:10:40 +0600

    Swagger Documentation Library added

--- src/Server.ts
+++ src/Server.ts
@@ -3,6 +3,10 @@ import bodyParser from 'body-parser';
 import { Constants } from './config/Constants';
 import { isAuthenticated } from './middlewares';
 import {Logger } from "./config/Logger";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+
+const swaggerDocument = YAML.load(__dirname + "/../swagger.yaml");
 
 const App = express();
 
@@ -14,6 +18,8 @@ App.use(bodyParser.json());
 /**
  * Rester Routers
  */
+App.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 global["logger"] = Logger
 
 export default App
